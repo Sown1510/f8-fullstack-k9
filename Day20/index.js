8a457874d9860c70f64a856bf7b18dd82b721b0f
Sown <bacongacon123@gmail.com>
2024-06-30 10:19:35 +0700

    update bt

--- Day20/index.js
+++ Day20/index.js
@@ -1,36 +1,29 @@
-let content = `<h1> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h1>`;
+let content = `<h1> Lorem ipsum dolor sit amet </h1>`;
 let newContent = " ";
 let count = 0;
 let start, end;
-let changeContent = [];
+let position = [];
+
 for (let i = 0; i < content.length; i++) {
   if (content[i] === " ") {
-    count++;
-    if (count < 3) {
-      if (count === 1) start = i;
-      if (count === 2) end = i;
-    } else {
-      start = end;
-      end = i;
-    }
-    if (start && end) {
-      console.log(start, end);
-      newContent =
-        content.slice(0, start) +
-        `<span>` +
-        content.slice(start, end) +
-        `</span>` +
-        content.slice(end);
-      changeContent.push(newContent);
-    }
+    position.push(i);
   }
 }
+console.log(position);
 
 let time = 0;
-let interVal = setInterval(function () {
-  if (time >= changeContent.length) {
+let show = setInterval(function () {
+  if (time >= position.length - 1) {
     time = 0;
   }
-  document.getElementById("content-container").innerHTML = changeContent[time];
+  let start = position[time];
+  let end = position[time + 1];
+  let newContent =
+    content.slice(0, start) +
+    `<span>` +
+    content.slice(start, end) +
+    `</span>` +
+    content.slice(end);
+  document.getElementById("content-container").innerHTML = newContent;
   time++;
 }, 1000);
