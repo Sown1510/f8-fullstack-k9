62c8f8758f5b3324af5183bc7a12ba28a8710e68
Sown <bacongacon123@gmail.com>
2024-07-06 01:31:37 +0700

    update bt

--- Day22/index.js
+++ Day22/index.js
@@ -1,17 +1,15 @@
-//Bài 1: Tìm giao giữa 2 mảng
-function arrayIntersection(arrA, arrB) {
-  let intersectionArray = [];
-  for (let i in arrA) {
-    for (let j in arrB) {
-      if (arrA[i] === arrB[j]) {
-        intersectionArray.push(arrA[i]);
-      }
+// Hàm dùng chung
+//Kiểm tra hàm số nguyên
+function checkInteger(array) {
+  for (let index in array) {
+    if (array[index] % 1 !== 0) {
+      return false;
     }
   }
-  intersectionArray = removeDuplicate(intersectionArray);
-  return intersectionArray;
+  return true;
 }
 
+//Xoá trùng lặp trong hàm
 function removeDuplicate(array) {
   for (let i = 0; i < array.length - 1; i++) {
     for (let j = i + 1; j < array.length; j++) {
@@ -23,6 +21,7 @@ function removeDuplicate(array) {
   return array;
 }
 
+//Xoá phần tử trong mảng
 function removeElement(array, removePosition) {
   let index = 0;
   let newArray = [];
@@ -39,7 +38,23 @@ function removeElement(array, removePosition) {
   return newArray;
 }
 
-console.log(arrayIntersection([1, 2, 3, 5, 1, 2], [2, 3, 1, 8, 9]));
+//Bài 1: Tìm giao giữa 2 mảng
+function arrayIntersection(arrA, arrB) {
+  let intersectionArray = [];
+  for (let i in arrA) {
+    for (let j in arrB) {
+      if (arrA[i] === arrB[j]) {
+        intersectionArray.push(arrA[i]);
+      }
+    }
+  }
+  intersectionArray = removeDuplicate(intersectionArray);
+  return intersectionArray;
+}
+
+let arrA = [1, 2, 3, 5, 1, 2];
+let arrB = [2, 3, 1, 8, 9];
+console.log(arrayIntersection(arrA, arrB));
 
 // Bài 2: Làm phẳng mảng
 let arr = [1, 2, [2, 3], 3];
@@ -62,16 +77,24 @@ function flatArray(array) {
 console.log(flatArray(arr));
 
 // Bài 3: Tách phần tử mảng theo đúng kiểu dữ liệu
-function findAndGroup(array) {
-  for (let i = 0; i < array.length; i++) {
-    let type = typeof array[i];
-    for (let j = i; j < array.length; j++) {
-      if (typeof array[j] === type) {
-        i++;
-        array = changePosition(array, i, j);
-      }
-    }
-  }
-}
 
-function changePosition(array, i, j) {}
+var array = [
+  ["a", 1, true],
+  ["b", 2, false],
+];
+let arrayOfTypes = [];
+array = array.flat();
+array.forEach(function (value) {
+  if (!arrayOfTypes.includes(typeof value)) {
+    arrayOfTypes.push(typeof value);
+  }
+});
+
+let newArray = [];
+arrayOfTypes.forEach(function (type, index) {
+  newArray[index] = array.filter(function (value) {
+    return typeof value === type;
+  });
+});
+
+console.log(newArray);
