5fc6c0cf663dd014b2db859231d97975729809b0
Sown <bacongacon123@gmail.com>
2024-08-07 20:37:13 +0700

    update bt

--- Day31/main.js
+++ Day31/main.js
@@ -25,7 +25,13 @@ var productList = [
     name: "Sản phẩm 5",
     price: 5000,
   },
+  {
+    id: 6,
+    name: "Sản phẩm 6",
+    price: 6000,
+  },
 ];
+var cart = [];
 productList.forEach((product, index) => {
   var tr = document.createElement("tr");
   var td = document.createElement("td");
@@ -68,50 +74,101 @@ function handleAddToCart(e) {
   updateCart();
 }
 
-var cartTable = document.querySelector(".table-2");
-var notice = document.querySelector(".empty-cart");
-function updateCart() {
-  if (cart) {
-    console.log(notice);
-    notice.style.display = "none";
-  }
-  while (cartTable.rows.length > 1) {
-    cartTable.deleteRow(1);
-  }
-  cart.forEach((product, index) => {
-    var tr = document.createElement("tr");
-    //stt
-    var stt = document.createElement("td");
-    stt.innerText = `${index + 1}`;
-    tr.append(stt);
-    //name
-    var productName = document.createElement("td");
-    productName.innerText = `${product.name}`;
-    tr.append(productName);
-    //price
-    var productPrice = document.createElement("td");
-    productPrice.innerText = `${product.price}`;
-    tr.append(productPrice);
-    //quantity
-    var productQuantity = document.createElement("td");
-    var input = document.createElement("input");
-    input.type = "number";
-    input.id = `${product.id}`;
-    input.value = `${product.quantity}`;
-    productQuantity.append(input);
-    tr.append(productQuantity);
-    //priceSum
-    var priceSum = document.createElement("td");
-    priceSum.innerText = `${product.quantity * product.price}`;
-    tr.append(priceSum);
-    //deleteButton
-    var deleteButton = document.createElement("td");
-    var button = document.createElement("button");
-    button.innerText = "Xoá";
-    deleteButton.append(button);
-    tr.append(deleteButton);
-    cartTable.append(tr);
-  });
+function handleRemoveFromCart(e) {
+  let productId = Number(e.target.id);
+  cart = cart.filter((product) => product.id != productId);
+  updateCart();
 }
 
-var cart = [];
+var productQuantEdit = [];
+var cartTable = document.querySelector(".table-2");
+var notice = document.querySelector(".empty-cart");
+cartTable.style.display = "none";
+function updateCart() {
+  if (cart.length != 0) {
+    notice.style.display = "none";
+    cartTable.style.display = "table";
+    while (cartTable.rows.length > 1) {
+      cartTable.deleteRow(1);
+    }
+    let totalQuant = 0;
+    let totalPrice = 0;
+    cart.forEach((product, index) => {
+      var tr = document.createElement("tr");
+      //stt
+      var stt = document.createElement("td");
+      stt.innerText = `${index + 1}`;
+      tr.append(stt);
+      //name
+      var productName = document.createElement("td");
+      productName.innerText = `${product.name}`;
+      tr.append(productName);
+      //price
+      var productPrice = document.createElement("td");
+      productPrice.innerText = `${product.price}`;
+      tr.append(productPrice);
+      //quantity
+      var productQuantity = document.createElement("td");
+      var input = document.createElement("input");
+      input.type = "number";
+      input.id = `${product.id}`;
+      input.classList = "quant_edit";
+      input.value = `${product.quantity}`;
+      productQuantity.append(input);
+      tr.append(productQuantity);
+      totalQuant += product.quantity;
+      //priceSum
+      var priceSum = document.createElement("td");
+      let totalCal = product.quantity * product.price;
+      priceSum.innerText = `${totalCal}`;
+      tr.append(priceSum);
+      totalPrice += totalCal;
+      //deleteButton
+      var deleteButton = document.createElement("td");
+      var button = document.createElement("button");
+      button.id = `${product.id}`;
+      button.addEventListener("click", handleRemoveFromCart);
+      button.innerText = "Xoá";
+      deleteButton.append(button);
+      tr.append(deleteButton);
+      cartTable.append(tr);
+    });
+    var totalRow = document.createElement("tr");
+    //Tiêu đề tổng
+    var td = document.createElement("td");
+    td.innerText = "Tổng";
+    td.colSpan = "3";
+    totalRow.append(td);
+    //tính tổng
+    var td = document.createElement("td");
+    td.innerText = `${totalQuant}`;
+    totalRow.append(td);
+    //Thành tiền
+    var td = document.createElement("td");
+    td.colSpan = "2";
+    td.innerText = `${totalPrice}`;
+    totalRow.append(td);
+    cartTable.append(totalRow);
+    productQuantEdit = document.querySelectorAll(".quant_edit");
+  } else {
+    notice.style.display = "block";
+    cartTable.style.display = "none";
+  }
+}
+
+var updateCartBtn = document.querySelector(".updateCart");
+var deleteCartBtn = document.querySelector(".deleteCart");
+updateCartBtn.addEventListener("click", handleUpdateCart);
+deleteCartBtn.addEventListener("click", handleDeleteCart);
+
+function handleDeleteCart() {
+  cart = [];
+  updateCart();
+}
+
+function handleUpdateCart() {
+  productQuantEdit.forEach((dataInput) => {
+    let dataId = Number(dataInput.value);
+    console.log(cart.find((product) => Number(product.id) === dataId));
+  });
+}
