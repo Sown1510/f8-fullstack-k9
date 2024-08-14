1abd2263b0cfb0a5665f0a406a84c68c3122478f
Sown <bacongacon123@gmail.com>
2024-08-14 15:29:28 +0700

    update bt

--- Day33/js/main.js
+++ Day33/js/main.js
@@ -3,6 +3,8 @@ var lessonContainer = document.querySelector(".container");
 var lessonListEl;
 var elementTarget;
 var shadowElementTarget;
+var offsetY;
+var offsetX;
 // Hiện thị danh sách bài học
 function renderLesson() {
   var lessonCount = 0;
@@ -26,18 +28,23 @@ renderLesson();
 // Hàm xử lý nhấn chuột vào 1 chủ để
 
 // Hàm xử lý hành động kéo 1 chủ đề
+
 function handleDrag(e) {
   elementTarget = e.currentTarget;
-  elementTarget.style.opacity = "0.5";
   shadowElementTarget = document.createElement("div");
   shadowElementTarget.classList = `${elementTarget.classList} shadow`;
   shadowElementTarget.innerHTML = `${elementTarget.innerHTML}`;
   shadowElementTarget.style.width = `${elementTarget.clientWidth}px`;
+  offsetY = e.clientY - elementTarget.offsetTop;
+  offsetX = e.clientX - elementTarget.offsetLeft;
   document.addEventListener("mousemove", handleMouseMove);
   document.addEventListener("mouseup", handleMouseUp);
 }
 
-function handleMouseMove() {
+function handleMouseMove(e) {
+  shadowElementTarget.style.top = `${e.clientY - offsetY}px`;
+  shadowElementTarget.style.left = `${e.clientX - offsetX}px`;
+  elementTarget.style.opacity = "0.5";
   lessonContainer.append(shadowElementTarget);
 }
 
