a16c8cca6b74c918bd456eccb273ba98ef605c22
Sown <bacongacon123@gmail.com>
2024-08-26 01:14:04 +0700

    update bt 36

--- Day36/js/main.js
+++ Day36/js/main.js
@@ -4,6 +4,7 @@ const listeningEl = document.querySelector(".listening");
 const listenedEl = document.querySelector(".listened");
 const statusPending = document.querySelector(".status.pending");
 const statusDone = document.querySelector(".status.done");
+const statusError = document.querySelector(".status.error");
 const keyWordEl = document.querySelector(".pending span");
 
 // Thêm sự kiện vào
@@ -12,26 +13,62 @@ voiceBtn.addEventListener("click", handleListening);
 // Hàm Xử lý nghe
 function handleListening() {
   listeningEl.classList.add("active");
+  listenedEl.classList.remove("active");
+  statusDone.classList.remove("active");
+  statusPending.classList.remove("active");
+  statusError.classList.remove("active");
+  if ("webkitSpeechRecognition" in window || "SpeechRecognition" in window) {
+    var keySearch = "";
+    const Speechrecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new Speechrecognition();
+
+    recognition.lang = "vi-VN";
+    recognition.interimResults = false;
+
+    recognition.onresult = (e) => {
+      const transcript = e.results[0][0].transcript;
+      keySearch = transcript;
+      statusError.classList.remove("active");
+      statusDone.classList.remove("active");
+      statusPending.classList.add("active");
+      listeningEl.classList.remove("active");
+      listenedEl.classList.add("active");
+      keyWordEl.innerText = keySearch;
+    };
+
+    recognition.onend = (e) => {
+      switch (keySearch) {
+        case "Google":
+          window.open("https://www.google.co.uk/");
+          handleStatus();
+          break;
+        case "Facebook":
+          window.open("https://www.facebook.com/");
+          handleStatus();
+          break;
+        case "YouTube":
+          window.open("https://www.youtube.com/");
+          handleStatus();
+          break;
+        case "Google Drive":
+          window.open("https://www.google.com/drive/");
+          handleStatus();
+          break;
+        default:
+          statusPending.classList.remove("active");
+          statusError.classList.add("active");
+          break;
+      }
+    };
+
+    recognition.start();
+  } else {
+    console.log("Không hỗ trợ");
+  }
 }
 
-if ("webkitSpeechRecognition" in window || "SpeechRecognition" in window) {
-  const Speechrecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
-  const recognition = new Speechrecognition();
-
-  recognition.lang = "vi-VN";
-  recognition.interimResults = false;
-
-  recognition.onresult = (e) => {
-    const transcript = e.result[0][0].transcript;
-    console.log("Bạn đã nói: ", transcript);
-  };
-
-  recognition.onend = (e) => {
-    console.log("kết thúc");
-  };
-
-  recognition.start();
-} else {
-  console.log("Không hỗ trợ");
+function handleStatus() {
+  statusPending.classList.remove("active");
+  statusDone.classList.add("active");
 }
