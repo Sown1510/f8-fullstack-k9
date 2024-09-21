7218a64864244a32f0d7a8649e1d9a0574f5ee6e
sown <bacongacon123@gmail.com>
2024-09-21 18:25:55 +0700

    update bt

--- Day41/main.js
+++ Day41/main.js
@@ -1,7 +1,7 @@
 import { openUserHome } from "./js/service.js";
 import { Login, Register } from "./js/views.js";
 
-export const router = new Navigo("/Day41/", { hash: true });
+export const router = new Navigo("/f8-fullstack-k9/Day41/", { hash: true });
 
 const app = document.getElementById("app");
 
