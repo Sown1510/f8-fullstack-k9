56c967dbe3e794cea4667665e2b32ca572cb39a7
Sown <bacongacon123@gmail.com>
2024-10-06 20:20:38 +0700

    update

--- Day44/react01/src/main.jsx
+++ Day44/react01/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App_old.jsx'
-// import './index.css'
+// import App from './App_old.jsx'
+import './index.css'
+
 import {
   createBrowserRouter,
   RouterProvider,
