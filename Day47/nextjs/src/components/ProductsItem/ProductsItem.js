d776445b9b89ab5744e436558328ad79fd19ad48
Sown <bacongacon123@gmail.com>
2024-10-31 00:48:51 +0700

    update

--- Day47/nextjs/src/components/ProductsItem/ProductsItem.js
+++ Day47/nextjs/src/components/ProductsItem/ProductsItem.js
@@ -6,9 +6,9 @@ export default function ProductsItem({ href, imgSrc, title, price, index }) {
   return (
     <li key={`product-${index}`} className={styles.product_banner}>
       <Link href={href}>
-        <img src={imgSrc} alt={title} />
-        <h3>{title}</h3>
-        <p>{price}</p>
+        <img className={styles.img} src={imgSrc} alt={title} />
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.price}>{price}</p>
       </Link>
     </li>
   );
