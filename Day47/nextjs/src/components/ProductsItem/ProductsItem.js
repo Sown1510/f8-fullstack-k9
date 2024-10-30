a59f6d2cba1eab7c84756e9073e066faf0219cf2
Sown <bacongacon123@gmail.com>
2024-10-31 01:06:15 +0700

    update

--- Day47/nextjs/src/components/ProductsItem/ProductsItem.js
+++ Day47/nextjs/src/components/ProductsItem/ProductsItem.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import styles from "./ProductsItem.module.css";
 
 export default function ProductsItem({ href, imgSrc, title, price, index }) {
-  console.log(href);
   return (
     <li key={`product-${index}`} className={styles.product_banner}>
       <Link href={href}>
